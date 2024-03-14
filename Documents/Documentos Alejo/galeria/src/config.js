a067fe05fe720fa15cf95e1aa21ca9f115f3c432
Alejandro Bautista <dalejandrobautista8@gmail.com>
2024-03-14 14:50:19 -0500

    primer commit

---
+++ Documents/Documentos Alejo/galeria/src/config.js
@@ -0,0 +1,3 @@
+// client/src/config.js
+
+export const API_URL = 'http://localhost:7000'; // Reemplaza con la URL real de tu servidor
---
+++ Documents/Documentos Alejo/galeria/src/principal.js
@@ -0,0 +1,16 @@
+function Principal() {
+    return (
+        <>
+            <div className="principio">
+                <img src="piedra.jpg" alt="piedra" className="imagen"></img>
+                <h1 className="voyage">Voyage</h1>
+            
+            <div className="madeBy">
+                By Alejandro Bautista
+            </div>
+            </div>
+        </>
+    );
+}
+
+export default Principal;
